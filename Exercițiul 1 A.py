269e3fc6f9c59d6e7abbde44c085853fd590a7c4
15Gaby07 <144488724+15Gaby07@users.noreply.github.com>
2023-09-27 00:14:57 +0300

    Add files via upload

---
+++ Exercițiul 1 A.py
@@ -0,0 +1,10 @@
+import math
+print (pow(6,7)/(pow(2,4)*pow(3,5))-(pow(2,48)/pow(2,18)+pow(3,20)+pow(6,23)/pow(6,13))/
+(pow(2,10)*pow(3,10)+pow(2,17)*pow(2,13)+pow(3,20)))
+print ((pow(4,5)*pow(2,13))/pow(8,7)+(pow(27,4)*pow(9,5))/pow(3,20))
+print (pow(5,103)/pow(5,68)*pow(5,32)+(324/pow(9,2)+5*pow(2,3))+pow(10,0))
+print ((pow(2,50)+pow(2,15)*pow(2,35)+pow(3,80)/pow(3,19))/(pow(3,60)+pow(3,60)+pow(3,60)+pow(2,70)/pow(2,19)))
+print (2*(math.sqrt(3+math.sqrt(5-math.sqrt(13+4*(math.sqrt(3)))))))
+print (math.sqrt(13+30*(math.sqrt(2+math.sqrt(9+4*(math.sqrt(2)))))))
+print ((math.sqrt(26+6*(math.sqrt(13-4*(math.sqrt(8+2*(math.sqrt(6-2*(math.sqrt(5))))))))))+
+(math.sqrt(26-6*(math.sqrt(13+4*(math.sqrt(8-2*(math.sqrt(6+2*math.sqrt(5))))))))))
